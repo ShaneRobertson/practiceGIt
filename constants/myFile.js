1bbe727555f8a8c14e197054c420694b9722a6e3
Shane Robertson <sjrobertson0812@gmail.com>
2021-08-02 10:32:41 -0400

    commiting the testing branch

---
+++ constants/myFile.js
@@ -0,0 +1,7 @@
+const myName = 'shane'
+const age = 23
+
+module.exports = {
+    myName,
+    age
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('starting a script....')
+
+
+const {myName} = require('./constants/myFile.js')
+console.log('myNameis: ', myName)
+
+console.log('ending the script...')
